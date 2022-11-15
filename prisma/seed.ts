2715af36b781b9045ea99aafa791c52b54b59962
uznam8x <uznam8x@uznam8xui-MacBookPro.local>
2022-11-15 17:40:40 +0900

    🤡 텍스트 파일 추가

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -17,13 +17,14 @@ async function main() {
   const record = [
     [1, 'directory', 'public', 0, 0, '', 'root', '', '', '', 1],
     [2, 'directory', 'public', 0, 1, '', '데스크톱', '', '', '', 1],
-    [3, 'file', 'public', 0, 2, '', 'cover.jpg', '', '{"url":"https://cdn.pixabay.com/photo/2022/10/19/01/18/cherry-blossom-7531346_1280.jpg","width":853,"height":1280}', '', 1],
-    [4, 'directory', 'public', 0, 2, '', '다운로드', '다운로드 파일', '', '', 1],
-    [5, 'directory', 'public', 0, 2, '', '날짜별', '날짜별 데이터', '', '', 1],
-    [6, 'directory', 'public', 0, 5, '', '10/16(일)', '', '', '', 1],
-    [7, 'directory', 'public', 0, 5, '', '10/15(토)', '', '', '', 1],
-    [8, 'directory', 'public', 0, 5, '', '10/14(금)', '', '', '', 1],
-    [9, 'file','public', 0, 5, '', 'cover.jpg', '', '{"url":"https://cdn.pixabay.com/photo/2022/10/19/01/18/cherry-blossom-7531346_1280.jpg","width":853,"height":1280}', '', 1],
+    [3, 'file', 'public', 0, 1, '', '텍스트.txt', '', '안녕하세요.', '', 1],
+    [4, 'file', 'public', 0, 1, '', 'sample.jpg', '', '{"url":"https://cdn.pixabay.com/photo/2022/10/19/01/18/cherry-blossom-7531346_1280.jpg","width":853,"height":1280}', '', 1],
+    [5, 'directory', 'public', 0, 2, '', '다운로드', '다운로드 파일', '', '', 1],
+    [6, 'directory', 'public', 0, 2, '', '날짜별', '날짜별 데이터', '', '', 1],
+    [7, 'directory', 'public', 0, 5, '', '10/16(일)', '', '', '', 1],
+    [8, 'directory', 'public', 0, 5, '', '10/15(토)', '', '', '', 1],
+    [9, 'directory', 'public', 0, 5, '', '10/14(금)', '', '', '', 1],
+    [10, 'file','public', 0, 5, '', 'cover.jpg', '', '{"url":"https://cdn.pixabay.com/photo/2022/10/19/01/18/cherry-blossom-7531346_1280.jpg","width":853,"height":1280}', '', 1],
   ];
 
   for (const item of record) {
